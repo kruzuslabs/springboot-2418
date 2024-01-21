6320c17338ed207dd3d0f966f7eeb8c66c238704
Bek <kruzus@live.com>
2024-01-21 05:10:06 -0500

    needs more work on task and user controllers

--- src/main/java/com/kruzuslabs/springcrud/Tasks/TaskController.java
+++ src/main/java/com/kruzuslabs/springcrud/Tasks/TaskController.java
@@ -1,5 +1,30 @@
 package com.kruzuslabs.springcrud.Tasks;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/tasks")
 public class TaskController {
-    
+
+    @Autowired
+    protected final TasksRepository tasksRepository;
+
+    public TaskController(TasksRepository tasksRepository) {
+        this.tasksRepository = tasksRepository;
+    }
+
+    @GetMapping("all")
+    public ResponseEntity<List<TaskEntity>> findAll() {
+        List<TaskEntity> tasks = this.tasksRepository.findAll();
+
+        return new ResponseEntity<List<TaskEntity>>(tasks, HttpStatus.OK);
+
+    }
 }
--- src/main/java/com/kruzuslabs/springcrud/Tasks/TaskEntity.java
+++ src/main/java/com/kruzuslabs/springcrud/Tasks/TaskEntity.java
@@ -12,7 +12,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "tasks", schema = "public", catalog = "skoozle")
+@Table(name = "tasks")
 public class TaskEntity {
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Id
@@ -42,6 +42,10 @@ public class TaskEntity {
     @Basic
     @Column(name = "created_at", nullable = true)
     private Date created_at;
+
+    public TaskEntity() {
+
+    }
 
     public TaskEntity(int id, String title, String content, boolean completed, short severity, UUID authUuid,
             Date created_at) {
@@ -108,58 +112,6 @@ public class TaskEntity {
 
     public void setCreated_at(Date created_at) {
         this.created_at = created_at;
-    }
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + id;
-        result = prime * result + ((title == null) ? 0 : title.hashCode());
-        result = prime * result + ((content == null) ? 0 : content.hashCode());
-        result = prime * result + (completed ? 1231 : 1237);
-        result = prime * result + severity;
-        result = prime * result + ((authUuid == null) ? 0 : authUuid.hashCode());
-        result = prime * result + ((created_at == null) ? 0 : created_at.hashCode());
-        return result;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        TaskEntity other = (TaskEntity) obj;
-        if (id != other.id)
-            return false;
-        if (title == null) {
-            if (other.title != null)
-                return false;
-        } else if (!title.equals(other.title))
-            return false;
-        if (content == null) {
-            if (other.content != null)
-                return false;
-        } else if (!content.equals(other.content))
-            return false;
-        if (completed != other.completed)
-            return false;
-        if (severity != other.severity)
-            return false;
-        if (authUuid == null) {
-            if (other.authUuid != null)
-                return false;
-        } else if (!authUuid.equals(other.authUuid))
-            return false;
-        if (created_at == null) {
-            if (other.created_at != null)
-                return false;
-        } else if (!created_at.equals(other.created_at))
-            return false;
-        return true;
     }
 
 }
