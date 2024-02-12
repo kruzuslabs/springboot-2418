a83b5c3990f919704485eae9f861bbb9a822cf46
Bek <kruzus@live.com>
2024-02-12 18:22:03 -0500

    working on user auth jwt

--- src/main/java/com/kruzuslabs/springcrud/Users/UserController.java
+++ src/main/java/com/kruzuslabs/springcrud/Users/UserController.java
@@ -1,10 +1,17 @@
 package com.kruzuslabs.springcrud.Users;
 
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.kruzuslabs.springcrud.Tickets.TicketsEntity;
+
 @RestController
-@RequestMapping("/users")
+@RequestMapping("/api/user")
 public class UserController {
 
 }
